59d88c72136d0ea9c26a8a41acb9fed0b1f0ec02
Gustav de Ridder <gustav.de.ridder@gmail.com>
2017-04-13 13:25:17 +0200

    Initial commit test1

---
+++ test1/main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Gustav de Ridder
+ *
+ * Created on 07 April 2017, 12:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("hallo world \n");
+    printf("it works");
+    return (EXIT_SUCCESS);
+}
+
