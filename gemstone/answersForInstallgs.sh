583b762796151f4ab7a7cb044eac689d8775e940
Gustav de Ridder <gustav.de.ridder@gmail.com>
2017-04-21 15:01:03 +0200

    initial commit of answersForInstallgs.sh

---
+++ gemstone/answersForInstallgs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+# answer the questions asked in the gemstone installation script $GEMSTONE/install/installgs
+
+echo ""
+echo ""
+echo "y" 
+echo "y" 
+echo "y" 
+echo ""
+echo "y"
+echo "vagrant"
+echo "vagrant"
+echo "y"
+echo "n"
+echo "n"
+echo "y"
+echo "y"
