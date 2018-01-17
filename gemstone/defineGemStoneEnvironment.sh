60150b900b0ae9cc95f1248a10236c1ff28b4424
gustav <gustav.de.ridder@gmail.com>
2018-01-17 11:57:57 +0200

    Remove script runTestsCi.sh.

--- gemstone/defineGemStoneEnvironment.sh
+++ gemstone/defineGemStoneEnvironment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-cat <<'EOF' >> $HOME/.profile
+export VAGRANT_HOME=/home/vagrant
+
+cat <<'EOF' >> $VAGRANT_HOME/.profile
     export GEMSTONE=/opt/gemstone/GemStone64Bit3.3.3-x86_64.Linux
     export LD_LIBRARY_PATH=$GEMSTONE/lib
     . $GEMSTONE/bin/gemsetup.sh  
