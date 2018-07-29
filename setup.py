ba1dcdd5369992ceccc49dfc8b8217c9bb5ff56e
Iwan Vosloo <iwan@reahl.org>
2018-07-29 17:10:11 +0200

    Added dependency on reahl-component (for Executable), and removed some C++ compiler warnings

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ gemstone_version = version_match.group(1)
 
 setup(
     name='parseltongue',
-    require=['reahl-component'],
+    install_requires=['reahl-component'],
     setup_requires=['cython','pytest-runner'],
     tests_require=['pytest', 'reahl-tofu'],
     packages=['ptongue'],
