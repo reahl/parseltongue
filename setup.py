20d2350512cfc60d7ac4d56980ac371678e6092a
Iwan Vosloo <iwan@reahl.org>
2021-07-22 20:17:28 +0200

    Removed cython left-overs from Dockerfile and setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='parseltongue',
     version='2.0.0',
     install_requires=['reahl-component', 'packaging'],
-    setup_requires=['cython','pytest-runner'],
+    setup_requires=['pytest-runner'],
     tests_require=['pytest', 'reahl-component', 'reahl-tofu'],
     packages=['ptongue']
     )
