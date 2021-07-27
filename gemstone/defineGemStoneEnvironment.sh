6d70d405a9cbe9222b1b6699fad027a57b2d6b7f
Craig Sparks <craig@reahl.org>
2021-07-27 09:54:41 +0200

    debugging

--- gemstone/defineGemStoneEnvironment.sh
+++ gemstone/defineGemStoneEnvironment.sh
@@ -9,9 +9,17 @@ VERSION=$1
 
 GEMSHELL=$(readlink -f $(dirname $0))/gemShell.sh
 
+echo "================== profile before =========="
+echo GEMSTONE1: $GEMSTONE
+cat $HOME/.profile
+echo "================== profile before end =========="
+
 cat <<EOF >> $HOME/.profile
-VERSION=$VERSION . $GEMSHELL 
+VERSION=$VERSION . $GEMSHELL
 echo GEMSTONE: $GEMSTONE
 EOF
 
-
+echo "================== profile after=========="
+echo GEMSTONE2: $GEMSTONE
+cat $HOME/.profile
+echo "================== profile after end=========="
